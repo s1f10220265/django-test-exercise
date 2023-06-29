52c3659e4e336d6ac04b9a0bb1b4f53c5b086962
1F10220265 <s1f102202651@iniad.org>
2023-06-29 12:12:22 +0900

    Taskモデルにメゾットを追加

--- todo/tests.py
+++ todo/tests.py
@@ -6,6 +6,10 @@ from todo.models import Task
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
+        self.assertEqual(1 + 2, 3)
+
+class TaskModelTestCase(TestCase):
+    def test_create_task1(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
         task = Task(title='task1', due_at=due)
         task.save()
@@ -23,4 +27,12 @@ class SampleTestCase(TestCase):
         self.assertEqual(task.title, 'task2')
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, None)
+
+    def test_is_overdue_future(self):
+        due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2023, 6, 30, 0, 0, 0))
+        task = Task(title='task1', due_at=due)
+        task.save()
+
+        self.assertFalse(task.is_overdue(current))
 
